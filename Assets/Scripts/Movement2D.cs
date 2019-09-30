cd75ad4610db274260ab5a295c673bf3f24522b5
Arvind-Maan <arvindmaan1199@gmail.com>
2019-09-29 21:53:56 -0500

    [Feature] Player learned 2 Jump!

--- Assets/Scripts/Movement2D.cs
+++ Assets/Scripts/Movement2D.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class Movement2D : MonoBehaviour {
     public float moveSpeed = 5f;
+    public float jumpHeight = 5f;
+    public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGrounded = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-    Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        Jump();
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += move * Time.deltaTime * moveSpeed;
-
+        
     }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+            isGrounded = false;
+    }
+
 }
