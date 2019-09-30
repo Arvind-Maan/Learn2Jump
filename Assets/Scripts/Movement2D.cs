c9197c4a734dfafbcb8314e768d75c6247b959af
Arvind-Maan <arvindmaan1199@gmail.com>
2019-09-29 21:15:08 -0500

    [Feature] Added left to right movement

---
+++ Assets/Scripts/Movement2D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour {
+    public float moveSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += move * Time.deltaTime * moveSpeed;
+
+    }
+}
