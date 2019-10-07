1040adb9e324d0bc2d1c62a3abf3fe01da9081b1
Arvind-Maan <arvindmaan1199@gmail.com>
2019-10-06 20:23:40 -0500

    Fixed Blocking Animation
    Made it so you can hold the blocking animation and it would transition into the hold and out of the hold smoothly

--- Assets/Scripts/Movement2D.cs
+++ Assets/Scripts/Movement2D.cs
@@ -12,44 +12,47 @@ public class Movement2D : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
+        //initialize variables;
         spriteRenderer = GetComponent<SpriteRenderer>();
         isGrounded = true;
+        isBlocking = true;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //check for any actions
         Jump();
+        Block();
+        //movement
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        //tell the animator we are moving
         animator.SetFloat("Speed", Mathf.Abs(move.x * moveSpeed));
+        //change position
         transform.position += move * Time.deltaTime * moveSpeed;
 
+        //flip sprite when we are moving left.
         if (move.x < 0)
             spriteRenderer.flipX = true;
         else if (move.x > 0)
             spriteRenderer.flipX = false;
-    }
+    } //end of update
 
     void Jump()
     {
         if (Input.GetButtonDown("Jump") && isGrounded)
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
-    }
+    } //end of jump
 
     void Block()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-        {
-            animator.SetBool("isBlocking", true);
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
             isBlocking = true;
-        }
         else
-        {
-            animator.SetBool("isBlocking", false);
             isBlocking = false;
-        }
-
-    }
+        animator.SetBool("Block", isBlocking);
+    } //end of block
 
 
 
